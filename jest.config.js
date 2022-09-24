0e6203ecda349a9ac50790d3d11620617db0ace9
Dorota Boinska <dboinska@gmail.com>
2022-09-24 23:32:51 +0200

    feat: initial config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['node_modules', '\\.cache', '.next', 'coverage', 'scripts'],
+};
+
+module.exports = createJestConfig(customJestConfig);
